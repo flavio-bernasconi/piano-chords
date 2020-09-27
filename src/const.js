1463ab4f6585dcc274930973fc774b6957a91356
MacBook <macbook@MBP-di-MacBook.homenet.telecomitalia.it>
2020-09-28 01:33:35 +0200

    create chords using a function

--- src/const.js
+++ src/const.js
@@ -19,103 +19,146 @@ export const OCTAVE_NOTES = OCTAVE_NUMBERS.reduce((notes, octaveNumber) => {
   return [...notes, ...notesInOctave];
 }, []);
 
-export const MAJOR = [
-  { "C major": ["C", "E", "G"] },
-  { "Db major": ["Db", "F", "Ab"] },
-  { "D major": ["D", "Gb", "A"] },
-  { "Eb major": ["Eb", "G", "Bb"] },
-  { "E major": ["E", "Ab", "B"] },
-  { "F major": ["F", "A", "C"] },
-  { "Gb major": ["Gb", "Bb", "Db"] },
-  { "G major": ["G", "B", "D"] },
-  { "Ab major": ["Ab", "C", "Eb"] },
-  { "A major": ["A", "Db", "E"] },
-  { "Bb major": ["Bb", "D", "F"] },
-  { "B major": ["B", "Eb", "Gb"] },
-];
+export function createTriadChord(
+  label,
+  secondNoteDistanceFromRoot,
+  thirdNoteDistanceSecondFromNote
+) {
+  return TONES.slice().map((root) => {
+    const rootIndex = TONES.indexOf(root);
+    return {
+      [`${root} ${label}`]: [
+        root,
+        TONES[(rootIndex + secondNoteDistanceFromRoot) % TONES.length],
+        TONES[
+          (rootIndex +
+            secondNoteDistanceFromRoot +
+            thirdNoteDistanceSecondFromNote) %
+            TONES.length
+        ],
+      ],
+    };
+  });
+}
 
-const MINOR = [
-  { "C minor": ["C", "Eb", "G"] },
-  { "Db minor": ["Db", "E", "Ab"] },
-  { "D minor": ["D", "F", "A"] },
-  { "Eb minor": ["Eb", "Ab", "Bb"] },
-  { "E minor": ["E", "G", "B"] },
-  { "F minor": ["F", "Ab", "C"] },
-  { "Gb minor": ["Gb", "A", "Db"] },
-  { "G minor": ["G", "Bb", "D"] },
-  { "Ab minor": ["Ab", "Db", "Eb"] },
-  { "A minor": ["A", "C", "E"] },
-  { "Bb minor": ["Bb", "Eb", "F"] },
-  { "B minor ": ["B", "D", "Gb"] },
-];
+export function createTetradChord(
+  label,
+  secondNoteDistanceFromRoot,
+  thirdNoteDistanceFromSecondNote,
+  fourthNoteDistanceFromThirdNote
+) {
+  return TONES.slice().map((root) => {
+    const rootIndex = TONES.indexOf(root);
+    return {
+      [`${root} ${label}`]: [
+        root,
+        TONES[(rootIndex + secondNoteDistanceFromRoot) % TONES.length],
+        TONES[
+          (rootIndex +
+            secondNoteDistanceFromRoot +
+            thirdNoteDistanceFromSecondNote) %
+            TONES.length
+        ],
+        TONES[
+          (rootIndex +
+            secondNoteDistanceFromRoot +
+            thirdNoteDistanceFromSecondNote +
+            fourthNoteDistanceFromThirdNote) %
+            TONES.length
+        ],
+      ],
+    };
+  });
+}
 
-const DIMINISHED = [
-  { "C diminished": ["C", "Eb", "Gb"] },
-  { "C# diminished": ["Db", "E", "G"] },
-  { "D diminished": ["D", "F", "Ab"] },
-  { "Eb diminished": ["Eb", "Gb", "A"] },
-  { "E diminished": ["E", "G", "Bb"] },
-  { "F diminished": ["F", "Ab", "B"] },
-  { "F# diminished": ["Gb", "A", "C"] },
-  { "G diminished": ["G", "Bb", "Db"] },
-  { "Ab diminished": ["Ab", "Cb", "D"] },
-  { "A diminished": ["A", "C", "Eb"] },
-  { "Bb diminished": ["Bb", "Db", "E"] },
-  { "B diminished": ["B", "D", "F"] },
-];
+export const MAJOR = createTriadChord("major", 4, 3);
 
-const MAJOR_SEVENTH = [
-  { "C major seventh": ["C", "E", "G", "B"] },
-  { "C# major seventh": ["Db", "F", "Ab", "C"] },
-  { "D major seventh": ["D", "Gb", "A", "Db"] },
-  { "D# major seventh": ["Eb", "G", "Bb", "D"] },
-  { "E major seventh": ["E", "Ab", "B", "Eb"] },
-  { "F major seventh": ["F", "A", "C", "E"] },
-  { "F# major seventh": ["Gb", "Bb", "Db", "F"] },
-  { "G major seventh": ["G", "B", "D", "Gb"] },
-  { "Ab major seventh": ["Ab", "C", "Eb", "G"] },
-  { "A major seventh": ["A", "Db", "E", "Ab"] },
-  { "Bb major seventh": ["Bb", "D", "F", "A"] },
-  { "B major seventh": ["B", "Eb", "Gb", "Bb"] },
-];
+const MINOR = createTriadChord("minor", 3, 4);
 
-const MINOR_SEVENTH = [
-  { "C minor seventh": ["C", "Eb", "G", "Bb"] },
-  { "C# minor seventh": ["Db", "E", "Ab", "B"] },
-  { "D minor seventh": ["D", "F", "A", "C"] },
-  { "Eb minor seventh": ["Eb", "Gb", "Bb", "Db"] },
-  { "E minor seventh": ["E", "G", "B", "D"] },
-  { "F minor seventh": ["F", "Ab", "C", "Eb"] },
-  { "F# minor seventh": ["Gb", "A", "Db", "E"] },
-  { "G minor seventh": ["G", "Bb", "D", "F"] },
-  { "Ab minor seventh": ["Ab", "B", "Eb", "Gb"] },
-  { "A minor seventh": ["A", "C", "E", "G"] },
-  { "Bb minor seventh": ["Bb", "Db", "F", "Ab"] },
-  { "B minor seventh": ["B", "D", "Gb", "A"] },
-];
+const DIMINISHED = createTriadChord("dim", 3, 3);
+
+const SUS = createTriadChord("sus", 5, 2);
+
+const AUG = createTriadChord("aug", 4, 4);
+
+const SEVENTH = createTetradChord("seventh", 4, 3, 3);
+
+const MAJOR_SEVENTH = createTetradChord("major seventh", 4, 3, 4);
+
+const MINOR_SEVENTH = createTetradChord("minor seventh", 3, 4, 3);
+
+const SIXTH = createTetradChord("sixth", 4, 3, 2);
+
+const MINOR_SIXTH = createTetradChord("minor sixth", 3, 4, 2);
+
+const ADD = createTetradChord("add", 4, 3, 7);
+
+const DIM_SEVENTH = createTetradChord("simished seventh", 3, 3, 3);
+
+const HALF = createTetradChord("half-diminished seventh", 3, 3, 4);
+
+// const MAJOR_SEVENTH = [
+//   { "C major seventh": ["C", "E", "G", "B"] },
+//   { "C# major seventh": ["Db", "F", "Ab", "C"] },
+//   { "D major seventh": ["D", "Gb", "A", "Db"] },
+//   { "D# major seventh": ["Eb", "G", "Bb", "D"] },
+//   { "E major seventh": ["E", "Ab", "B", "Eb"] },
+//   { "F major seventh": ["F", "A", "C", "E"] },
+//   { "F# major seventh": ["Gb", "Bb", "Db", "F"] },
+//   { "G major seventh": ["G", "B", "D", "Gb"] },
+//   { "Ab major seventh": ["Ab", "C", "Eb", "G"] },
+//   { "A major seventh": ["A", "Db", "E", "Ab"] },
+//   { "Bb major seventh": ["Bb", "D", "F", "A"] },
+//   { "B major seventh": ["B", "Eb", "Gb", "Bb"] },
+// ];
+
+// const MINOR_SEVENTH = [
+//   { "C minor seventh": ["C", "Eb", "G", "Bb"] },
+//   { "C# minor seventh": ["Db", "E", "Ab", "B"] },
+//   { "D minor seventh": ["D", "F", "A", "C"] },
+//   { "Eb minor seventh": ["Eb", "Gb", "Bb", "Db"] },
+//   { "E minor seventh": ["E", "G", "B", "D"] },
+//   { "F minor seventh": ["F", "Ab", "C", "Eb"] },
+//   { "F# minor seventh": ["Gb", "A", "Db", "E"] },
+//   { "G minor seventh": ["G", "Bb", "D", "F"] },
+//   { "Ab minor seventh": ["Ab", "B", "Eb", "Gb"] },
+//   { "A minor seventh": ["A", "C", "E", "G"] },
+//   { "Bb minor seventh": ["Bb", "Db", "F", "Ab"] },
+//   { "B minor seventh": ["B", "D", "Gb", "A"] },
+// ];
+// const MINOR_SEVENTH_FLAT = [
+//   { "C minor seventh flat five": ["C", "Eb", "Gb", "Bb"] },
+//   { "C# minor seventh flat five": ["Db", "E", "G", "B"] },
+//   { "D minor seventh flat five": ["D", "F", "Ab", "C"] },
+//   { "Eb minor seventh flat five": ["Eb", "Gb", "A", "Db"] },
+//   { "E minor seventh flat five": ["E", "G", "Bb", "D"] },
+//   { "F minor seventh flat five": ["F", "Ab", "B", "Eb"] },
+//   { "F# minor seventh flat five": ["Gb", "A", "C", "E"] },
+//   { "G minor seventh flat five": ["G", "Bb", "Db", "F"] },
+//   { "Ab minor seventh flat five": ["Ab", "Cb", "D", "Gb"] },
+//   { "A minor seventh flat five": ["A", "C", "Eb", "G"] },
+//   { "Bb minor seventh flat five": ["Bb", "Db", "E", "Ab"] },
+//   { "B minor seventh flat five": ["B", "D", "F", "A"] },
+// ];
 //C# = Db F# = Gb G# = Ab
-const MINOR_SEVENTH_FLAT = [
-  { "C minor seventh flat five": ["C", "Eb", "Gb", "Bb"] },
-  { "C# minor seventh flat five": ["Db", "E", "G", "B"] },
-  { "D minor seventh flat five": ["D", "F", "Ab", "C"] },
-  { "Eb minor seventh flat five": ["Eb", "Gb", "A", "Db"] },
-  { "E minor seventh flat five": ["E", "G", "Bb", "D"] },
-  { "F minor seventh flat five": ["F", "Ab", "B", "Eb"] },
-  { "F# minor seventh flat five": ["Gb", "A", "C", "E"] },
-  { "G minor seventh flat five": ["G", "Bb", "Db", "F"] },
-  { "Ab minor seventh flat five": ["Ab", "Cb", "D", "Gb"] },
-  { "A minor seventh flat five": ["A", "C", "Eb", "G"] },
-  { "Bb minor seventh flat five": ["Bb", "Db", "E", "Ab"] },
-  { "B minor seventh flat five": ["B", "D", "F", "A"] },
-];
 
 export const CHORDS = [
   ...MAJOR,
   ...MINOR,
   ...DIMINISHED,
+  ...SUS,
+  ...AUG,
+  ...SEVENTH,
   ...MAJOR_SEVENTH,
   ...MINOR_SEVENTH,
-  ...MINOR_SEVENTH_FLAT,
+  ...SIXTH,
+  ...MINOR_SIXTH,
+  ...ADD,
+  ...DIM_SEVENTH,
+  ...HALF,
+  // ...MAJOR_SIXTH,
+  // ...MINOR_SEVENTH,
+  // ...MINOR_SEVENTH_FLAT,
 ];
 
 export const timeNotesPlay = 1300;
